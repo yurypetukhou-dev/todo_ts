bccb6aa1f35f9450448a915d67f2d8582245aa1a
Yury <y.petukhou@exadel.com>
2020-01-13 17:32:41 +0300

    Added Header

---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+export const Header: React.FC = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <NavLink to="/" className="brand-logo">Logo</NavLink>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><NavLink to="/">Main page</NavLink></li>
+                    <li><NavLink to="/add">Add todo</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+
+
